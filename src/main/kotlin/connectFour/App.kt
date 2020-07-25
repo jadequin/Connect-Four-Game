15d60dff980835eb4f8e6780b4c3527b926aff34
Till Nitsche <till.nitsche@mni.thm.de>
2020-07-25 18:30:52 +0200

    -improved alpha beta

--- src/main/kotlin/connectFour/App.kt
+++ src/main/kotlin/connectFour/App.kt
@@ -5,6 +5,7 @@ package connectFour
 import kotlin.io.writeText
 import kotlin.io.readText
 import java.io.File
+import kotlin.io.readLine
 
 
 fun main() {
@@ -17,12 +18,16 @@ fun main() {
     println(c4)
 
     while(!c4.isGameOver()) {
-        c4 = if(c4.isP1Turn()) c4.makeBestMove() else c4.listMoves().random()
+        c4 = if(c4.isP1Turn())
+            c4.makeBestMove()
+        else
+            c4.listMoves()[(readLine() ?: "0").toInt()]
+//            c4.listMoves().random()
         println(c4)
         println()
     }
 
-    //database output with pattern of: P1Hash:P2Hash;Result
-//    val dbText = ConnectFour.results.entries.joinToString(separator = ",") { "${it.key.first}:${it.key.second};${it.value}" }
+    //database output with pattern of: P1Hash:P2Hash:turn;Result
+//    val dbText = ConnectFour.results.entries.joinToString(separator = ",") { "${it.key.first}:${it.key.second}:${it.key.third};${it.value}" }
 //    File("src/main/kotlin/connectFour/connect4DB.txt").writeText(dbText)
 }
--- src/main/kotlin/connectFour/ConnectFour.kt
+++ src/main/kotlin/connectFour/ConnectFour.kt
@@ -10,20 +10,23 @@ import kotlin.math.sign
 class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = listOf(0, 7, 14, 21, 28, 35, 42), val turn: Int = +1, val previousBoard: ConnectFour? = null) {
 
     companion object {
-        private const val DEPTH = 5
-        private const val MC_SIMULATIONS = 100
+        private const val DEPTH = 3
+        private const val MC_SIMULATIONS = 1000
 
-        private val boardPositions = listOf(5,12,19,26,33,40,47,4,11,18,25,32,39,46,3,10,17,24,31,38,45,2,9,16,23,30,37,44,1,8,15,22,29,36,43,0,7,14,21,28,35,42)
-        val results = hashMapOf<Pair<Long, Long>, Int>()
+        val results = hashMapOf<Triple<Long, Long, Int>, Int>()
 
         fun parseDatabase(data: String) {
+            if(data.isEmpty())
+                return
+
             val entries = data.split(",")
-            val p1Hashes = entries.map { it.split(";")[0].split(":")[0] }
-            val p2Hashes = entries.map { it.split(";")[0].split(":")[1] }
-            val values = entries.map { it.split(";")[1] }
+            val p1Hashes = entries.map { it.split(";")[0].split(":")[0].toLong() }
+            val p2Hashes = entries.map { it.split(";")[0].split(":")[1].toLong() }
+            val turn = entries.map { it.split(";")[0].split(":")[2].toInt() }
+            val values = entries.map { it.split(";")[1].toInt() }
 
             entries.indices.forEach {
-                results[Pair(p1Hashes[it].toLong(), p2Hashes[it].toLong())] = values[it].toInt()
+                results[Triple(p1Hashes[it], p2Hashes[it], turn[it])] = values[it]
             }
         }
     }
@@ -53,25 +56,28 @@ class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = li
 
     fun result() = if(isWinPreviousPlayer()) -turn else if(isOutOfMoves()) 0 else turn
 
-    fun isOutOfMoves() = listMoves().isEmpty()
+    fun isOutOfMoves() = (141845657554976 and board()) xor 141845657554976 == 0L //compare with top row
 
     fun isGameOver() = isWinPreviousPlayer() || isOutOfMoves()
 
-    //Quelle: https://github.com/denkspuren/playerC4/blob/master/playerDesign.md
+    //Quelle: https://github.com/denkspuren/BitboardC4/blob/master/BitboardDesign.md
     fun isWinPreviousPlayer(): Boolean {
-        val player = if(turn == -1) p1 else p2
-        return listOf(1,7,6,8).any { player and (player shr it) and (player shr (it * 2)) and (player shr (it * 3)) != 0L }
+        val bitboard = if(turn == -1) p1 else p2
+        return listOf(1,7,6,8).any { val bb = bitboard and (bitboard shr it); (bb and (bb shr (2 * it))) != 0L }
     }
 
     fun alphaBeta(depth: Int = DEPTH, alpha: Int = -Int.MAX_VALUE, beta: Int = Int.MAX_VALUE): Int {
-        if(results[Pair(mirrorHash(p1), mirrorHash(p2))] != null)
-            return results[Pair(mirrorHash(p1), mirrorHash(p2))]!! * -turn
+
+        val hashKey = Triple(hashing(p1), hashing(p2), turn)
+
+        if(results[hashKey] != null)
+            return results[hashKey]!!
 
         if(isGameOver())
-            return result() * (depth + 1)
+            return result() * (depth + 1) * 100_000
 
         if(depth == 0)
-            return monteCarloTreeSearch()
+            return monteCarloTreeSearch() * (depth + 1)
 
         //alpha-beta-implementation combined with negamax
         val bestScore = run {
@@ -83,23 +89,19 @@ class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = li
                 return@fold max(bestScore, score)
             }
         }
-        results[Pair(mirrorHash(p1), mirrorHash(p2))] = bestScore * -turn
+        results[hashKey] = bestScore
         return bestScore
     }
 
     fun randomEndGame(): ConnectFour = if(isGameOver()) this else listMoves().random().randomEndGame()
 
-    fun simulatePlays() = (1..MC_SIMULATIONS).sumBy { randomEndGame().result() * turn }
+    fun monteCarloTreeSearch() = (1..MC_SIMULATIONS).sumBy { val randGame = randomEndGame(); randGame.result() * if(randGame.turn == this.turn) 1 else -1 }.sign
 
-    fun monteCarloTreeSearch() = listMoves().map { it.simulatePlays() }.max()!!.sign
-
-    fun mirrorHash(player: Long) = min(
-            player,
-            ((63L shl 0 and player) shl 42) + ((63L shl 7 and player) shl 28) + ((63L shl 14 and player) shl 14) + ((63L shl 21 and player)) + ((63L shl 28 and player) shr 14) + ((63L shl 35 and player) shr 28) + ((63L shl 42 and player) shr 42)
-    )
-
+    fun hashing(player: Long) = min(player, ((63L and player) shl 42) + ((8064L and player) shl 28) + ((1032192L and player) shl 14) + ((132120576L and player)) + ((16911433728L and player) shr 14) + ((2164663517184L and player) shr 28) + ((277076930199552L and player) shr 42))
 
     override fun toString(): String {
+        val boardPositions = listOf(5,12,19,26,33,40,47,4,11,18,25,32,39,46,3,10,17,24,31,38,45,2,9,16,23,30,37,44,1,8,15,22,29,36,43,0,7,14,21,28,35,42)
+
         return boardPositions.indices.joinToString(separator = "") {
             (if(it % 7 == 0 && it != 0) "\n" else "") + (if(((p1 shr boardPositions[it]) and 1L) == 1L) "X" else if(((p2 shr boardPositions[it]) and 1L) == 1L) "O" else ".")
         }
