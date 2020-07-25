81e7974ed59abfb5f57c82455a6b25878949b906
Till Nitsche <till.nitsche@mni.thm.de>
2020-07-25 11:22:29 +0200

    added database

--- src/main/kotlin/connectFour/App.kt
+++ src/main/kotlin/connectFour/App.kt
@@ -2,9 +2,27 @@
 
 package connectFour
 
+import kotlin.io.writeText
+import kotlin.io.readText
+import java.io.File
 
 
 fun main() {
+    val data = File("src/main/kotlin/connectFour/connect4DB.txt").readText()
+    ConnectFour.parseDatabase(data)
+
+    println(ConnectFour.results.values.max())
+
     var c4 = ConnectFour()
-    println(c4.alphaBeta())
+    println(c4)
+
+    while(!c4.isGameOver()) {
+        c4 = if(c4.isP1Turn()) c4.makeBestMove() else c4.listMoves().random()
+        println(c4)
+        println()
+    }
+
+    //database output with pattern of: P1Hash:P2Hash;Result
+//    val dbText = ConnectFour.results.entries.joinToString(separator = ",") { "${it.key.first}:${it.key.second};${it.value}" }
+//    File("src/main/kotlin/connectFour/connect4DB.txt").writeText(dbText)
 }
--- src/main/kotlin/connectFour/ConnectFour.kt
+++ src/main/kotlin/connectFour/ConnectFour.kt
@@ -1,21 +1,38 @@
 package connectFour
 
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sign
 
 
 //https://de.wikipedia.org/wiki/Vier_gewinnt
 
-class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = listOf(0, 7, 14, 21, 28, 35, 42), val turn: Int = +1, val history: List<ConnectFour> = emptyList()) {
+class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = listOf(0, 7, 14, 21, 28, 35, 42), val turn: Int = +1, val previousBoard: ConnectFour? = null) {
 
     companion object {
-        private const val DEPTH = 3
+        private const val DEPTH = 5
         private const val MC_SIMULATIONS = 100
+
+        private val boardPositions = listOf(5,12,19,26,33,40,47,4,11,18,25,32,39,46,3,10,17,24,31,38,45,2,9,16,23,30,37,44,1,8,15,22,29,36,43,0,7,14,21,28,35,42)
+        val results = hashMapOf<Pair<Long, Long>, Int>()
+
+        fun parseDatabase(data: String) {
+            val entries = data.split(",")
+            val p1Hashes = entries.map { it.split(";")[0].split(":")[0] }
+            val p2Hashes = entries.map { it.split(";")[0].split(":")[1] }
+            val values = entries.map { it.split(";")[1] }
+
+            entries.indices.forEach {
+                results[Pair(p1Hashes[it].toLong(), p2Hashes[it].toLong())] = values[it].toInt()
+            }
+        }
     }
 
     private fun board() = p1 or p2
 
     fun isP1Turn() = turn == 1
+
+    fun makeBestMove(): ConnectFour = listMoves().maxBy { it.alphaBeta() }!!
 
     fun isValidMove(col: Int): Boolean = ((board() shr (col + 1) * 7 - 1) and 1L) == 0L //returns true if the specified column within 0 to 6 is free
 
@@ -28,11 +45,11 @@ class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = li
                 p2 = p2 + if(turn == -1) newPos else 0,
                 height = height.take(col) + listOf(height[col] + 1) + height.drop(col + 1),
                 turn = -turn,
-                history = history.plus(this)
+                previousBoard = this
         )
     }
 
-    fun undo(times: Int = 1) = if(history.isEmpty()) this else history.takeLast(times).first()
+    fun undo(times: Int = 1): ConnectFour = if(previousBoard == null || times == 0) this else previousBoard.undo(times - 1)
 
     fun result() = if(isWinPreviousPlayer()) -turn else if(isOutOfMoves()) 0 else turn
 
@@ -47,12 +64,11 @@ class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = li
     }
 
     fun alphaBeta(depth: Int = DEPTH, alpha: Int = -Int.MAX_VALUE, beta: Int = Int.MAX_VALUE): Int {
-//        if(results[this] != null)
-//            return results[this]!! * -turn
-
+        if(results[Pair(mirrorHash(p1), mirrorHash(p2))] != null)
+            return results[Pair(mirrorHash(p1), mirrorHash(p2))]!! * -turn
 
         if(isGameOver())
-            return result()
+            return result() * (depth + 1)
 
         if(depth == 0)
             return monteCarloTreeSearch()
@@ -67,24 +83,25 @@ class ConnectFour(val p1: Long = 0, val p2: Long = 0, val height: List<Int> = li
                 return@fold max(bestScore, score)
             }
         }
-//        results[this] = bestScore * -turn
+        results[Pair(mirrorHash(p1), mirrorHash(p2))] = bestScore * -turn
         return bestScore
     }
 
     fun randomEndGame(): ConnectFour = if(isGameOver()) this else listMoves().random().randomEndGame()
 
-    fun simulatePlays() = (1..MC_SIMULATIONS).sumBy {
-        val randomLateGame = randomEndGame()
-        return@sumBy randomLateGame.result() * turn
-    }
+    fun simulatePlays() = (1..MC_SIMULATIONS).sumBy { randomEndGame().result() * turn }
 
     fun monteCarloTreeSearch() = listMoves().map { it.simulatePlays() }.max()!!.sign
 
+    fun mirrorHash(player: Long) = min(
+            player,
+            ((63L shl 0 and player) shl 42) + ((63L shl 7 and player) shl 28) + ((63L shl 14 and player) shl 14) + ((63L shl 21 and player)) + ((63L shl 28 and player) shr 14) + ((63L shl 35 and player) shr 28) + ((63L shl 42 and player) shr 42)
+    )
+
 
     override fun toString(): String {
-        val positions = listOf(5,12,19,26,33,40,47,4,11,18,25,32,39,46,3,10,17,24,31,38,45,2,9,16,23,30,37,44,1,8,15,22,29,36,43,0,7,14,21,28,35,42)
-        return positions.indices.joinToString(separator = "") {
-            (if(it % 7 == 0 && it != 0) "\n" else "") + (if(((p1 shr positions[it]) and 1L) == 1L) "X" else if(((p2 shr positions[it]) and 1L) == 1L) "O" else ".")
+        return boardPositions.indices.joinToString(separator = "") {
+            (if(it % 7 == 0 && it != 0) "\n" else "") + (if(((p1 shr boardPositions[it]) and 1L) == 1L) "X" else if(((p2 shr boardPositions[it]) and 1L) == 1L) "O" else ".")
         }
     }
 }
